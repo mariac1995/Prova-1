2750c50dd941e5e4265bdc07e4637b2f86a938e2
mariac1995 <mcrosasantana@gmail.com>
2025-03-09 14:06:55 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,11 @@
+#Você está criando um programa para calcular a área de um retângulo. 
+#O programa deve solicitar ao usuário que forneça a base e a
+#altura do retângulo. Em seguida, o programa deve calcular a área usando a fórmula:
+#Área=Base×Altura
+#O programa deve exibir a área calculada.
+
+altura = int(input("Digite a sua altura do seu retangulo: "))
+base = int(input("Digite a sua base do seu retangulo: "))
+
+area = base * altura
+print(f"O valor da area é {area}")
